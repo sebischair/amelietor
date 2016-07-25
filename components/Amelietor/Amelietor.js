1afb899498f273a148861e8d4940bed9cf5a9124
Klim Shumaiev <klimksh@gmail.com>
2016-07-25 10:04:57 +0200

    annotations added

--- components/Amelietor/Amelietor.js
+++ components/Amelietor/Amelietor.js
@@ -2,72 +2,206 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import cx from 'classnames';
 import s from './Amelietor.css';
-import {Editor, EditorState, RichUtils } from 'draft-js';
+import {
+  Editor,
+  EditorState,
+  RichUtils,
+  convertFromRaw,
+  convertToRaw,
+  CompositeDecorator,
+  ContentState,
+  Entity
+  } from 'draft-js';
+
+const rawContent = {
+  blocks: [
+    {
+      text: (
+        'This is an "immutable" entity: Superman. Deleting any ' +
+        'characters will delete the entire entity. Adding characters ' +
+        'will remove the entity from the range.'
+      ),
+      type: 'unstyled',
+      entityRanges: [{offset: 31, length: 8, key: 'first'}],
+    },
+    {
+      text: '',
+      type: 'unstyled',
+    },
+    {
+      text: (
+        'This is a "mutable" entity: Batman. Characters may be added ' +
+        'and removed.'
+      ),
+      type: 'unstyled',
+      entityRanges: [{offset: 28, length: 6, key: 'second'}],
+    },
+    {
+      text: '',
+      type: 'unstyled',
+    },
+    {
+      text: (
+        'This is a "segmented" entity: Green Lantern. Deleting any ' +
+        'characters will delete the current "segment" from the range. ' +
+        'Adding characters will remove the entire entity from the range.'
+      ),
+      type: 'unstyled',
+      entityRanges: [{offset: 30, length: 13, key: 'third'}],
+    },
+  ],
+
+  entityMap: {
+    first: {
+      type: 'TOKEN',
+      mutability: 'IMMUTABLE',
+    },
+    second: {
+      type: 'TOKEN',
+      mutability: 'MUTABLE',
+    },
+    third: {
+      type: 'TOKEN',
+      mutability: 'SEGMENTED',
+    },
+  },
+};
 
 class Amelietor extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {editorState: EditorState.createEmpty()};
+
     this.onChange = (editorState) => this.setState({editorState});
-    this.toggleInlineStyle = (style) => this._toggleInlineStyle(style);
-    this.handleKeyCommand = this.handleKeyCommand.bind(this);
     this.focus = () => this.refs.editor.focus();
 
+    this.logState = () => {
+      const content = this.state.editorState.getCurrentContent();
+      console.log(convertToRaw(content));
+    };
+
+
+    const blocks = convertFromRaw(rawContent);
+
+    const decorator = new CompositeDecorator([
+      {
+        strategy: getEntityStrategy('IMMUTABLE'),
+        component: TokenSpan,
+      },
+      {
+        strategy: getEntityStrategy('MUTABLE'),
+        component: TokenSpan,
+      },
+      {
+        strategy: getEntityStrategy('SEGMENTED'),
+        component: TokenSpan,
+      },
+    ]);
+
+    this.state = {editorState: EditorState.createWithContent(blocks, decorator)};
+
+
+    this.state = {
+      editorState: EditorState.createWithContent(blocks, decorator),
+    };
+
   }
 
-  //componentDidMount() {
-  //  window.componentHandler.upgradeElement(this.root);
-  //}
-  //
-  //componentWillUnmount() {
-  //  window.componentHandler.downgradeElements(this.root);
-  //}
 
-  handleKeyCommand(command) {
-    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
-    if (newState) {
-      this.onChange(newState);
-      return true;
-    }
-    return false;
-  }
-
-  _toggleInlineStyle(inlineStyle) {
-    this.onChange(
-      RichUtils.toggleInlineStyle(
-        this.state.editorState,
-        inlineStyle
-      )
-    );
-  }
 
   render() {
     const {editorState} = this.state;
-    let className = 'RichEditor-editor';
-    var contentState = editorState.getCurrentContent();
-    if (!contentState.hasText()) {
-      if (contentState.getBlockMap().first().getType() !== 'unstyled') {
-        className += ' RichEditor-hidePlaceholder';
-      }
-    }
-
     return (
-
-    <div className={`${s["RichEditor-root"]}`}>
-        <div className={className} onClick={this.focus}>
+      <div>
+        <div className={`${s.editor}`}>
           <Editor
-            placeholder="Place you content ..."
             editorState={editorState}
             handleKeyCommand={this.handleKeyCommand}
             onChange={this.onChange}
+            ref="editor"
             spellCheck={true}
           />
         </div>
-    </div>
+        <input
+          onClick={this.logState}
+          className={`mdl-button mdl-js-button mdl-button--accent ${s.button}`}
+          type="button"
+          value="Log State"
+        />
+      </div>
     );
   }
+
+
 }
+
+function getEntityStrategy(mutability) {
+  return function(contentBlock, callback) {
+    contentBlock.findEntityRanges(
+      (character) => {
+        const entityKey = character.getEntity();
+        if (entityKey === null) {
+          return false;
+        }
+        return Entity.get(entityKey).getMutability() === mutability;
+      },
+      callback
+    );
+  };
+
+}
+
+function getDecoratedStyle(mutability) {
+  switch (mutability) {
+    case 'IMMUTABLE': return styles.immutable;
+    case 'MUTABLE': return styles.mutable;
+    case 'SEGMENTED': return styles.segmented;
+    default: return null;
+  }
+}
+
+const TokenSpan = (props) => {
+  const style = getDecoratedStyle(
+    Entity.get(props.entityKey).getMutability()
+  );
+  return (
+    <span {...props} style={style}>
+            {props.children}
+          </span>
+  );
+};
+
+const styles = {
+  root: {
+    fontFamily: '\'Helvetica\', sans-serif',
+    padding: 20,
+    width: 600,
+  },
+  editor: {
+    border: '1px solid #ccc',
+    cursor: 'text',
+    minHeight: 80,
+    padding: 10,
+  },
+  button: {
+    marginTop: 10,
+    textAlign: 'center',
+  },
+  immutable: {
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    padding: '2px 0',
+  },
+  mutable: {
+    backgroundColor: 'rgba(204, 204, 255, 1.0)',
+    padding: '2px 0',
+  },
+  segmented: {
+    backgroundColor: 'rgba(248, 222, 126, 1.0)',
+    padding: '2px 0',
+  },
+};
+
+
 
 
 export default Amelietor;
