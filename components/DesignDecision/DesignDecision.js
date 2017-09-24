3f48864d969fa2f5537b744432735b61b01aa15a
Klim Shumaiev <klimksh@gmail.com>
2017-09-24 22:37:09 +0200

    formatted code

--- components/DesignDecision/DesignDecision.js
+++ components/DesignDecision/DesignDecision.js
@@ -1,11 +1,11 @@
-import React, {PropTypes} from 'react';
-import {connect} from 'react-redux';
-import {Card, CardTitle, CardText, CardActions, Tabs, Tab, Button, FABButton, Icon, Grid, Cell} from 'react-mdl';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { Card, CardTitle, CardText, CardActions, Tabs, Tab, Button, FABButton, Icon, Grid, Cell } from 'react-mdl';
 import history from '../../src/history';
 import HelperFunctions from '../HelperFunctions';
-import {fetchSelctedProject, fetchSelctedDD} from '../../core/actions/scactions';
+import { fetchSelctedProject, fetchSelctedDD } from '../../core/actions/scactions';
 import Amelietor from '../Amelietor'
-import {receiveFileContent} from '../../core/actions/actions';
+import { receiveFileContent } from '../../core/actions/actions';
 import s from './DesignDecision.css';
 import TokenManager from "../TokenManager";
 import RecContainer from "../RecContainer";
@@ -14,7 +14,7 @@ import EditorControls from "../EditorControls";
 class DesignDecision extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {projectId: "", ddId: ""};
+    this.state = { projectId: "", ddId: "" };
 
     this.state.projectId = this.props.projectId;
     this.state.ddId = this.props.id;
@@ -29,7 +29,7 @@ class DesignDecision extends React.Component {
       entityMap: {
       }
     };
-     
+
     if (Object.keys(this.props.selectedDD).length === 0 && this.props.selectedDD.constructor === Object) {
       this.props.dispatch(fetchSelctedDD(this.state.ddId));
     }
@@ -46,37 +46,37 @@ class DesignDecision extends React.Component {
   componentWillReceiveProps(nextProps) {
     if (nextProps.selectedDD.hasOwnProperty("description") && nextProps.selectedProject.hasOwnProperty("projectId")) {
       this.props.dispatch(receiveFileContent(null, [nextProps.selectedDD.description], true));
-      this.setState({summary:nextProps.selectedDD.summary})
+      this.setState({ summary: nextProps.selectedDD.summary })
     }
   }
 
   onBackArrow = () => {
     history.push({
-      pathname: '/recommender/'+ this.state.projectId + '/dd'
+      pathname: '/recommender/' + this.state.projectId + '/dd'
     });
   };
 
   render() {
     return (
-          <div className="mdl-grid">
-            <div className="mdl-cell mdl-cell--8-col">
-              <h3>{this.state.summary}</h3>
-              <Amelietor triggerOnLoad={true} initialContent={this.rawContent} readOnly={true}/>
-            </div>
-            <div className={`mdl-cell mdl-cell--4-col ${s.recommendations}`}>
-              <TokenManager/>
-              <br/>
-              <RecContainer />
-            </div>
-            <EditorControls />
-          </div>
+      <div className="mdl-grid">
+        <div className="mdl-cell mdl-cell--8-col">
+          <h3>{this.state.summary}</h3>
+          <Amelietor triggerOnLoad={true} initialContent={this.rawContent} readOnly={true} />
+        </div>
+        <div className={`mdl-cell mdl-cell--4-col ${s.recommendations}`}>
+          <TokenManager />
+          <br />
+          <RecContainer />
+        </div>
+        <EditorControls />
+      </div>
     );
   }
 }
 
 const mapStateToProps = (state) => {
-  const {selectedDD, selectedProject} = state.screcs;
-  return {selectedDD, selectedProject};
+  const { selectedDD, selectedProject } = state.screcs;
+  return { selectedDD, selectedProject };
 };
 
 DesignDecision.propTypes = {
