f5bfbd7296784853140b73da8ca86841882c7011
Jiping Xu <xujiping2003@gmail.com>
2018-03-18 18:10:25 +0100

    #9: reimplement expert recommendations table with pagination

--- components/Experts/Experts.js
+++ components/Experts/Experts.js
@@ -1,83 +1,152 @@
-import React, {PropTypes} from 'react';
-import {connect} from 'react-redux';
-import {Textfield} from 'react-mdl';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { withStyles } from 'material-ui/styles';
+import TextField from 'material-ui/TextField';
+import { CircularProgress } from 'material-ui/Progress';
+import Paper from 'material-ui/Paper';
+import Table, { TableBody, TableCell, TableHead, TableRow, TableFooter, TablePagination } from 'material-ui/Table';
+import List, { ListItem, ListItemText } from 'material-ui/List';
+
 import HelperFunctions from '../HelperFunctions';
-import {fetchSelctedProject, fetchERData} from '../../core/actions/scactions';
+import { fetchSelctedProject, fetchERData } from '../../core/actions/scactions';
 import history from '../../src/history';
 import s from './Experts.css';
-import {Spinner} from 'react-mdl';
+
+const styles = {
+  listItem: {
+    paddingLeft: 0
+  }
+};
 
 class Experts extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {searchString: ""};
-    let projectKey = this.props.projectKey === undefined ? HelperFunctions.getParameterByName("projectKey", history.location.search) : this.props.projectKey;
+    this.state = {
+      searchString: '',
+      page: 0,
+      rowsPerPage: 10
+    };
+    let projectKey =
+      this.props.projectKey === undefined
+        ? HelperFunctions.getParameterByName('projectKey', history.location.search)
+        : this.props.projectKey;
     if (Object.keys(this.props.selectedProject).length === 0 && this.props.selectedProject.constructor === Object) {
       this.props.dispatch(fetchSelctedProject(projectKey));
     }
 
-    if(this.props.erData.length === 0) {
+    if (this.props.erData.length === 0) {
       this.props.dispatch(fetchERData(projectKey));
     }
   }
 
-  handleChange = (event) => {
-    this.setState({searchString: event.target.value});
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
+
+  handleChangeRowsPerPage = event => {
+    this.setState({ rowsPerPage: event.target.value });
+  };
+
+  handleChangeSearch = event => {
+    this.setState({ searchString: event.target.value });
   };
 
   render() {
-    let adds = this.props.erData.filter(e => {
-      if(e.predictions) return e.predictions.length > 0;
+    let issues = this.props.erData.filter(e => {
+      if (e.predictions) return e.predictions.length > 0;
       else return false;
     });
     let searchString = this.state.searchString.trim().toLowerCase();
+    const { rowsPerPage, page } = this.state;
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, issues.length - page * rowsPerPage);
+
     if (searchString.length > 0) {
-      adds = adds.filter(add => {
-        return (add.text.toLowerCase().indexOf(searchString) !== -1);
+      issues = issues.filter(add => {
+        return add.text.toLowerCase().indexOf(searchString) !== -1;
       });
     }
 
     //TODO: Unable to set the width of columns
     return (
       <div>
-        <Textfield value={this.state.searchString} onChange={this.handleChange} label="Search..."
-                   style={{width: '400px'}}/>
+        <TextField
+          value={this.state.searchString}
+          onChange={this.handleChangeSearch}
+          label="Search open decisions..."
+          className={s.searchField}
+        />
+        <br />
+        <br />
+        <div className={s.circularProgress}>{issues.length === 0 && <CircularProgress />}</div>
 
-        <div style={{'textAlign': 'center'}}> {adds.length === 0 && <Spinner /> } </div>
-
-        <table className={`mdl-data-table mdl-js-data-table mdl-shadow--2dp ${s.customWidth}`}>
-          <thead>
-          <tr>
-            <th className="mdl-data-table__cell--non-numeric"><b>Open Design Decisions</b></th>
-            <th><b>Experts Recommendation</b></th>
-          </tr>
-          </thead>
-          <tbody>
-            {adds.map(function(add, i){
-              return <tr key={i}>
-                <td className={`mdl-data-table__cell--non-numeric ${s.customWidthTR}`}>{ add.text }</td>
-                <td>
-                  {add.predictions.map(function(pre, j) {
-                    return <p key={j}>{pre.personName} -- {pre.score}</p>
-                  })}
-                </td>
-              </tr>
-            })}
-          </tbody>
-        </table>
+        <Paper>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Open Design Decisions</TableCell>
+                <TableCell className={s.expertsColumn}>Experts Recommendation</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {issues.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((issue, indexOfIssue) => {
+                return (
+                  <TableRow hover key={indexOfIssue}>
+                    <TableCell>{issue.text}</TableCell>
+                    <TableCell>
+                      <List dense={true}>
+                        {issue.predictions.map((pre, indexOfPre) => {
+                          return (
+                            <ListItem key={indexOfPre} className={this.props.classes.listItem}>
+                              <ListItemText primary={pre.personName + ' — ' + pre.score} />
+                            </ListItem>
+                          );
+                        })}
+                      </List>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 49 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  colSpan={6}
+                  count={issues.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  backIconButtonProps={{
+                    'aria-label': 'Previous Page'
+                  }}
+                  nextIconButtonProps={{
+                    'aria-label': 'Next Page'
+                  }}
+                  onChangePage={this.handleChangePage}
+                  onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                />
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </Paper>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  const {selectedProject, erData} = state.screcs;
-  return {selectedProject, erData};
+const mapStateToProps = state => {
+  const { selectedProject, erData } = state.screcs;
+  return { selectedProject, erData };
 };
 
 Experts.propTypes = {
   erData: PropTypes.array.isRequired,
+  projectKey: PropTypes.string,
+  selectedProject: PropTypes.object,
   dispatch: PropTypes.func.isRequired
 };
 
-export default connect(mapStateToProps)(Experts);
+export default connect(mapStateToProps)(withStyles(styles)(Experts));
