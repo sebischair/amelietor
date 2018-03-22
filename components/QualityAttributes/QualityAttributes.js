228e5f565398dcfd7a9375c030ac2e46add22a75
Jiping Xu <xujiping2003@gmail.com>
2018-03-23 00:47:52 +0100

    #13: replace deprecated react-mdl

--- components/QualityAttributes/QualityAttributes.js
+++ components/QualityAttributes/QualityAttributes.js
@@ -1,65 +1,84 @@
-import React, {PropTypes} from 'react';
-import {connect} from 'react-redux';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import Grid from 'material-ui/Grid';
+import { CircularProgress } from 'material-ui/Progress';
+
 import history from '../../src/history';
 import HelperFunctions from '../HelperFunctions';
-import {fetchSelctedProject, fetchQAData} from '../../core/actions/scactions';
+import { fetchSelctedProject, fetchQAData } from '../../core/actions/scactions';
 import StackedBarChart from '../StackedBarChart/StackedBarChart';
-import {Spinner, Grid, Cell} from 'react-mdl';
+import s from './QualityAttributes.css';
 
 class QualityAttributes extends React.Component {
   constructor(props) {
     super(props);
-    let projectKey = this.props.projectKey === undefined ? HelperFunctions.getParameterByName("projectKey", history.location.search) : this.props.projectKey;
-    if (Object.keys(this.props.selectedProject).length === 0 && this.props.selectedProject.constructor === Object) {
-      this.props.dispatch(fetchSelctedProject(projectKey));
-    }
+    this.state = {
+      projectKey: this.props.projectKey
+    };
 
+    if (this.props.projectKey === undefined) {
+      this.setState({ projectKey: HelperFunctions.getParameterByName('projectKey', history.location.search) });
+    }
+    if (Object.keys(this.props.selectedProject).length === 0 && this.props.selectedProject.constructor === Object) {
+      this.props.dispatch(fetchSelctedProject(this.state.projectKey));
+    }
     if (this.props.qaData.length === 0) {
-      this.props.dispatch(fetchQAData(projectKey));
+      this.props.dispatch(fetchQAData(this.state.projectKey));
     }
   }
 
   render() {
-    let emptyDDList = "";
-    if(this.props.qaData.length > 0) {
-      emptyDDList = <div className="mdl-card__supporting-text">
+    let emptyDDList = '';
+    if (this.props.qaData.length > 0) {
+      emptyDDList = (
+        <div className="mdl-card__supporting-text">
           <b>Missing Quality Attributes</b>
           <ul>
-          {
-            this.props.qaData.map(qa => {
+            {this.props.qaData.map(qa => {
               if (qa.value.reduce((a, b) => a + b, 0) == 0) {
-                return <li key={qa.id}> {qa.id} </li>
+                return <li key={qa.id}> {qa.id} </li>;
               }
-            })
-          }
+            })}
           </ul>
-      </div>
+        </div>
+      );
     }
 
     return (
-      <Grid>
-        <Cell col={10}>
-          <div>
-            <div style={{'textAlign': 'center'}}>
-              {this.props.qaData.length === 0 && <Spinner /> }
-            </div>
-            {this.props.qaData.length > 0 && <StackedBarChart data={this.props.qaData} changeTabHandler={this.props.changeTabHandler} viz="qa"/>}
+      <div>
+        {this.props.qaData.length === 0 && (
+          <div className={s.circularProgress}>
+            <CircularProgress />
+            <br />
+            <br />
           </div>
-        </Cell>
-        <Cell col={2}>
-          {emptyDDList}
-        </Cell>
-      </Grid>
+        )}
+        <Grid container>
+          <Grid item xs={10}>
+            <div>
+              {this.props.qaData.length > 0 && (
+                <StackedBarChart data={this.props.qaData} changeTabHandler={this.props.changeTabHandler} viz="qa" />
+              )}
+            </div>
+          </Grid>
+          <Grid item xs={2}>
+            {emptyDDList}
+          </Grid>
+        </Grid>
+      </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  const {selectedProject, qaData} = state.screcs;
-  return {selectedProject, qaData};
+const mapStateToProps = state => {
+  const { selectedProject, qaData } = state.screcs;
+  return { selectedProject, qaData };
 };
 
 QualityAttributes.propTypes = {
+  projectKey: PropTypes.string,
+  selectedProject: PropTypes.object,
+  changeTabHandler: PropTypes.func,
   qaData: PropTypes.array.isRequired,
   dispatch: PropTypes.func.isRequired
 };
