dc73214a309d0ce1900810da184df2e5031b9582
Jiping Xu <xujiping2003@gmail.com>
2018-03-23 11:51:44 +0100

    #15: replace components and refactor

--- components/ArchitecturalElements/ArchitecturalElements.js
+++ components/ArchitecturalElements/ArchitecturalElements.js
@@ -1,42 +1,58 @@
-import React, {PropTypes} from 'react';
-import {connect} from 'react-redux';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { CircularProgress } from 'material-ui/Progress';
+
 import history from '../../src/history';
 import HelperFunctions from '../HelperFunctions';
-import {fetchSelctedProject, fetchAEData} from '../../core/actions/scactions';
+import { fetchSelctedProject, fetchAEData } from '../../core/actions/scactions';
 import BubbleChart from '../BubbleChart/BubbleChart';
-import {Spinner} from 'react-mdl';
+import s from './ArchitecturalElements.css';
 
 class ArchitecturalElements extends React.Component {
   constructor(props) {
     super(props);
-    let projectKey = this.props.projectKey === undefined ? HelperFunctions.getParameterByName("projectKey", history.location.search) : this.props.projectKey;
-    if (Object.keys(this.props.selectedProject).length === 0 && this.props.selectedProject.constructor === Object) {
-      this.props.dispatch(fetchSelctedProject(projectKey));
-    }
+    this.state = {
+      projectKey: this.props.projectKey
+    };
 
-    if(this.props.aeData.length === 0) {
-      this.props.dispatch(fetchAEData(projectKey));
+    if (this.props.projectKey === undefined) {
+      this.setState({ projectKey: HelperFunctions.getParameterByName('projectKey', history.location.search) });
+    }
+    if (Object.keys(this.props.selectedProject).length === 0 && this.props.selectedProject.constructor === Object) {
+      this.props.dispatch(fetchSelctedProject(this.state.projectKey));
+    }
+    if (this.props.aeData.length === 0) {
+      this.props.dispatch(fetchAEData(this.state.projectKey));
     }
   }
 
   render() {
     return (
-    <div>
-      <div style={{'textAlign': 'center'}}>
-        {this.props.aeData.length === 0 && <Spinner /> }
+      <div>
+        {this.props.aeData.length === 0 && (
+          <div className={s.circularProgress}>
+            <CircularProgress />
+            <br />
+            <br />
+          </div>
+        )}
+        {this.props.aeData.length > 0 && (
+          <BubbleChart data={this.props.aeData} changeTabHandler={this.props.changeTabHandler} viz="ae" />
+        )}
       </div>
-      {this.props.aeData.length > 0 && <BubbleChart data={this.props.aeData} changeTabHandler={this.props.changeTabHandler} viz="ae" />}
-    </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  const {selectedProject, aeData} = state.screcs;
-  return {selectedProject, aeData};
+const mapStateToProps = state => {
+  const { selectedProject, aeData } = state.screcs;
+  return { selectedProject, aeData };
 };
 
 ArchitecturalElements.propTypes = {
+  projectKey: PropTypes.string,
+  selectedProject: PropTypes.object,
+  changeTabHandler: PropTypes.func,
   aeData: PropTypes.array.isRequired,
   dispatch: PropTypes.func.isRequired
 };
--- components/BubbleChart/BubbleChart.js
+++ components/BubbleChart/BubbleChart.js
@@ -1,50 +1,58 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import * as d3 from 'd3';
 import rd3 from 'react-d3-library';
+import { Slider } from 'react-mdl';
+
 import d3BubbleChart from './d3BubbleChart';
 import s from './BubbleChart.css';
-import {Slider, Grid, Cell} from 'react-mdl';
-import * as d3 from 'd3';
+
+const minYear = 2013;
+const maxYear = 2017;
+const defaultYear = 2017;
+const defaultHeight = 640;
+const defaultWidth = 960;
 
 class BubbleChart extends React.Component {
-
   constructor(props) {
     super(props);
-    this.state = {d3: '', year: 2017};
+    this.state = { d3: '', year: defaultYear };
   }
 
   componentDidMount() {
     let changeTabHandler = this.props.changeTabHandler;
     let viz = this.props.viz;
-    this.setState({d3: d3BubbleChart.getNode(this.props.data)});
-    d3.selectAll('circle').on("click", (d) => {
-      changeTabHandler(4, viz, d.id, "default");
+    this.setState({ d3: d3BubbleChart.getNode(this.props.data) });
+    d3.selectAll('circle').on('click', d => {
+      changeTabHandler(4, viz, d.id, 'default');
     });
   }
 
-  redraw = (event) => {
-    this.setState({d3: d3BubbleChart.redraw(this.props.data, event.target.value), year: event.target.value});
+  redraw = event => {
+    this.setState({ d3: d3BubbleChart.redraw(this.props.data, event.target.value), year: event.target.value });
   };
 
   render() {
     const RD3Component = rd3.Component;
-    let year = this.state.year;
+
     return (
-      <Grid>
-        <Cell col={12}>
-          <div className={`${s.bubbleChart}`}>
-            {this.state.d3 && <Slider min={2013} max={2017} defaultValue={2017} onChange={this.redraw} />}
-            {year}
-          </div>
-        </Cell>
-        <Cell col={12}>
-          <div className={`${s.bubbleChart}`}>
-            <svg height="640" width="960" className="bubble" id="svg" ></svg>
-            {this.state.d3 && <RD3Component data={this.state.d3}/>}
-          </div>
-        </Cell>
-      </Grid>
+      <div>
+        <div className={`${s.bubbleChart}`}>
+          {this.state.d3 && <Slider min={minYear} max={maxYear} defaultValue={defaultYear} onChange={this.redraw} />}
+          {this.state.year}
+        </div>
+        <div className={`${s.bubbleChart}`}>
+          <svg height={defaultHeight} width={defaultWidth} className="bubble" id="svg" />
+          {this.state.d3 && <RD3Component data={this.state.d3} />}
+        </div>
+      </div>
     );
   }
 }
+
+BubbleChart.propTypes = {
+  data: PropTypes.array.isRequired,
+  changeTabHandler: PropTypes.func,
+  viz: PropTypes.string
+};
 
 export default BubbleChart;
