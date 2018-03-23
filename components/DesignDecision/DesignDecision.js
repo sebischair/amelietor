41ce7ebf84f2b2b0327b08d523b31b2923178f33
Jiping Xu <xujiping2003@gmail.com>
2018-03-23 22:18:47 +0100

    refactor state and grid layout

--- components/DesignDecision/DesignDecision.js
+++ components/DesignDecision/DesignDecision.js
@@ -1,55 +1,61 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { withStyles } from 'material-ui/styles';
+import Grid from 'material-ui/Grid';
+
 import history from '../../src/history';
+import Amelietor from '../Amelietor';
+import TokenManager from '../TokenManager';
+import RecContainer from '../RecContainer';
+import EditorControls from '../EditorControls';
+import SimilarDocuments from '../SimilarDocuments';
 import { fetchSelctedProject, fetchSelctedDD } from '../../core/actions/scactions';
-import Amelietor from '../Amelietor'
 import { receiveFileContent } from '../../core/actions/actions';
 import s from './DesignDecision.css';
-import TokenManager from "../TokenManager";
-import RecContainer from "../RecContainer";
-import EditorControls from "../EditorControls";
-import SimilarDocuments from "../SimilarDocuments"
+
+const styles = {
+  gridContainer: {
+    margin: 0
+  }
+};
 
 class DesignDecision extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { projectKey: "", issueKey: "" };
-    this.state.projectKey = this.props.projectKey;
-    this.state.issueKey = this.props.issueKey;
-    this.state.summary = this.props.selectedDD.summary;
+    this.state = {
+      projectKey: this.props.projectKey,
+      issueKey: this.props.issueKey,
+      summary: this.props.selectedDD.summary,
+      similarDocuments: []
+    };
     this.rawContent = {
       blocks: [
         {
-          text: (this.props.selectedDD.description || ''),
-          type: 'unstyled',
+          text: this.props.selectedDD.description || '',
+          type: 'unstyled'
         }
       ],
-      entityMap: {
-      }
+      entityMap: {}
     };
-
-    this.state.similarDocuments = [];
-    if(this.props.selectedDD.similarDocuments !== undefined && this.props.selectedDD.similarDocuments !== null) {
-      this.state.similarDocuments = this.props.selectedDD.similarDocuments;
-    }
-
-    if (Object.keys(this.props.selectedDD).length === 0 && this.props.selectedDD.constructor === Object) {
-      this.props.dispatch(fetchSelctedDD(this.state.issueKey));
-    }
 
     if (Object.keys(this.props.selectedProject).length === 0 && this.props.selectedProject.constructor === Object) {
       this.props.dispatch(fetchSelctedProject(this.state.projectKey));
     }
-
-    if (this.props.selectedDD.hasOwnProperty("description") && this.props.selectedProject.hasOwnProperty("key")) {
+    if (Object.keys(this.props.selectedDD).length === 0 && this.props.selectedDD.constructor === Object) {
+      this.props.dispatch(fetchSelctedDD(this.state.issueKey));
+    }
+    if (this.props.selectedDD.hasOwnProperty('description') && this.props.selectedProject.hasOwnProperty('key')) {
       this.props.dispatch(receiveFileContent(null, [this.props.selectedDD.description], true));
+    }
+    if (this.props.selectedDD.similarDocuments !== undefined && this.props.selectedDD.similarDocuments !== null) {
+      this.state.similarDocuments = this.props.selectedDD.similarDocuments;
     }
   }
 
   componentWillReceiveProps(nextProps) {
-    if (nextProps.selectedDD.hasOwnProperty("description") && nextProps.selectedProject.hasOwnProperty("key")) {
+    if (nextProps.selectedDD.hasOwnProperty('description') && nextProps.selectedProject.hasOwnProperty('key')) {
       this.props.dispatch(receiveFileContent(null, [nextProps.selectedDD.description], true));
-      this.setState({ summary: nextProps.selectedDD.summary })
+      this.setState({ summary: nextProps.selectedDD.summary });
     }
   }
 
@@ -61,30 +67,35 @@ class DesignDecision extends React.Component {
 
   render() {
     return (
-      <div className="mdl-grid">
-        <div className="mdl-cell mdl-cell--7-col">
+      <Grid container className={this.props.classes.gridContainer}>
+        <Grid item xs={7}>
           <h3>{this.state.summary}</h3>
           <Amelietor triggerOnLoad={true} initialContent={this.rawContent} readOnly={true} />
-        </div>
-        <div className={`mdl-cell mdl-cell--5-col ${s.recommendations}`}>
+          <EditorControls />
+        </Grid>
+        <Grid item xs={5} className={s.recommendations}>
           <TokenManager />
           <br />
           <RecContainer />
-          <SimilarDocuments similarDocuments={this.state.similarDocuments}/>
-        </div>
-        <EditorControls />
-      </div>
+          <SimilarDocuments similarDocuments={this.state.similarDocuments} />
+        </Grid>
+      </Grid>
     );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   const { selectedDD, selectedProject } = state.screcs;
   return { selectedDD, selectedProject };
 };
 
 DesignDecision.propTypes = {
+  projectKey: PropTypes.string.isRequired,
+  issueKey: PropTypes.string.isRequired,
+  selectedProject: PropTypes.object,
+  selectedDD: PropTypes.object,
+  classes: PropTypes.object,
   dispatch: PropTypes.func.isRequired
 };
 
-export default connect(mapStateToProps)(DesignDecision);
+export default connect(mapStateToProps)(withStyles(styles)(DesignDecision));
