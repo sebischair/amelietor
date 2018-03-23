684d086aa2a376c622624690b4bfe53811eabe78
Jiping Xu <xujiping2003@gmail.com>
2018-03-23 21:33:40 +0100

    #18: remove UploadZone from EditorControls

--- components/EditorControls/EditorControls.js
+++ components/EditorControls/EditorControls.js
@@ -1,28 +1,26 @@
-import React, {PropTypes} from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import UploadZone from '../UploadZone';
 
-import { Button, Icon, ProgressBar} from 'react-mdl';
+import { Button, Icon, ProgressBar } from 'react-mdl';
 
 import s from './EditorControls.css';
-import {decorate} from "../../core/actions/amelietorActions";
+import { decorate } from '../../core/actions/amelietorActions';
 
 class EditorControls extends React.Component {
   constructor(props) {
     super(props);
-    const {dispatch} = this.props;
     this.annotate = () => {
       this.props.dispatch(decorate());
     };
     this.state = {
       allFetched: true,
       noErrors: true
-    }
+    };
   }
-  componentWillReceiveProps(nextProps){
+  componentWillReceiveProps(nextProps) {
     let annotations_list = [];
-    Object.keys(nextProps.annotations).forEach(function (key) {
-      annotations_list.push(nextProps.annotations[key])
+    Object.keys(nextProps.annotations).forEach(function(key) {
+      annotations_list.push(nextProps.annotations[key]);
     });
     const checkAllFetched = (element, index, array) => {
       return element.isFetching === false;
@@ -30,31 +28,49 @@ class EditorControls extends React.Component {
     const checkNoErrors = (element, index, array) => {
       return element.isError === false;
     };
-    this.setState({allFetched:annotations_list.every(checkAllFetched), noErrors:annotations_list.every(checkNoErrors)});
+    this.setState({
+      allFetched: annotations_list.every(checkAllFetched),
+      noErrors: annotations_list.every(checkNoErrors)
+    });
   }
   render() {
-    const {allFetched, noErrors} = this.state;
+    const { allFetched, noErrors } = this.state;
     return (
       <div className={`${s.controls}`}>
-        {allFetched && <Button ripple onClick={e => { e.preventDefault(); this.annotate()}}><Icon name="refresh" /> Annotate </Button>}
+        {allFetched && (
+          <Button
+            ripple
+            onClick={e => {
+              e.preventDefault();
+              this.annotate();
+            }}
+          >
+            <Icon name="refresh" /> Annotate{' '}
+          </Button>
+        )}
         {!allFetched && <ProgressBar indeterminate />}
-        {!allFetched && <i>Processing... </i> }
-        {!noErrors && <Button raised accent ripple> <Icon name="report" /> Errors occurred. Show logs</Button>}
-        <UploadZone />
+        {!allFetched && <i>Processing... </i>}
+        {!noErrors && (
+          <Button raised accent ripple>
+            {' '}
+            <Icon name="report" /> Errors occurred. Show logs
+          </Button>
+        )}
       </div>
-    )
+    );
   }
 }
 
 EditorControls.propTypes = {
-  annotations: PropTypes.object.isRequired
+  annotations: PropTypes.object.isRequired,
+  dispatch: PropTypes.func.isRequired
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   const annotations = state.annotationsByKey;
   return {
     annotations
-  }
+  };
 };
 
-export default connect(mapStateToProps)(EditorControls)
+export default connect(mapStateToProps)(EditorControls);
--- components/ExampleEditor/ExampleEditor.js
+++ components/ExampleEditor/ExampleEditor.js
@@ -1,74 +1,60 @@
-import React, {PropTypes} from 'react';
-let jsSHA = require("jssha");
-import {selectRec, fetchAnnotationsPerBlock, selectKey, fetchSession} from '../../core/actions/actions';
-import {connect} from 'react-redux'
-
-import {Textfield} from 'react-mdl';
+import React from 'react';
+import { Textfield } from 'react-mdl';
 
 import Amelietor from '../Amelietor';
-import TokenManager from '../TokenManager'
+import TokenManager from '../TokenManager';
 import RecContainer from '../RecContainer';
-import EditorControls from '../EditorControls'
-
+import EditorControls from '../EditorControls';
+import UploadZone from '../UploadZone';
 import s from './ExampleEditor.css';
 
 const rawContent = {
   blocks: [
     {
-      text: (
+      text:
         'The Yummy Inc online application will be deployed onto a ' +
         'J2EE application server Websphere Application Server version 6, ' +
-        'as it is already the application server use for internal applications.'
-      ),
-      type: 'unstyled',
-    },
-    {
-      text: '',
-      type: 'unstyled',
-    },
-    {
-      text: (
-        'J2EE security model will be reused. ' +
-        'Data persistence will be addressed using a relational database.'
-      ),
+        'as it is already the application server use for internal applications.',
       type: 'unstyled'
     },
     {
       text: '',
-      type: 'unstyled',
+      type: 'unstyled'
     },
+    {
+      text: 'J2EE security model will be reused. ' + 'Data persistence will be addressed using a relational database.',
+      type: 'unstyled'
+    },
+    {
+      text: '',
+      type: 'unstyled'
+    }
   ],
-  entityMap: {
-  },
+  entityMap: {}
 };
 
 class ExampleEditor extends React.Component {
-
   constructor(props) {
     super(props);
-  };
+  }
 
   render() {
     return (
-        <div className="mdl-grid">
-          <div className="mdl-cell mdl-cell--8-col">
-            <Textfield
-              className={`${s.textfield}`}
-              onChange={() => {}}
-              label="Document name"
-            />
-            <Amelietor initialContent={rawContent} />
-          </div>
-          <EditorControls />
-          <div className={`mdl-cell mdl-cell--4-col ${s.recommendations}`}>
-            <TokenManager/>
-            <br/>
-            <RecContainer />
-          </div>
+      <div className="mdl-grid">
+        <div className="mdl-cell mdl-cell--8-col">
+          <Textfield className={`${s.textfield}`} onChange={() => {}} label="Document name" />
+          <Amelietor initialContent={rawContent} />
         </div>
+        <EditorControls />
+        <UploadZone />
+        <div className={`mdl-cell mdl-cell--4-col ${s.recommendations}`}>
+          <TokenManager />
+          <br />
+          <RecContainer />
+        </div>
+      </div>
     );
   }
 }
 
 export default ExampleEditor;
-
