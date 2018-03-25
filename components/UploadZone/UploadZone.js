06fc0f5635aef324b5220554ec053d62bb1e83ef
Jiping Xu <xujiping2003@gmail.com>
2018-03-25 18:03:31 +0200

    #19: change UploadZone styles

--- components/UploadZone/UploadZone.js
+++ components/UploadZone/UploadZone.js
@@ -1,17 +1,18 @@
-import React, { PropTypes, Component} from 'react'
-import { connect } from 'react-redux'
-import {uploadFile} from '../../core/actions/actions';
-import Dropzone from 'react-dropzone'
+import React, { PropTypes, Component } from 'react';
+import { connect } from 'react-redux';
+import Dropzone from 'react-dropzone';
+
+import { uploadFile } from '../../core/actions/actions';
 import s from './UploadZone.css';
 
 class UploadZone extends Component {
   constructor(props) {
     super(props);
-    const {dispatch} = this.props;
-    this.onDrop = (files) => {
+    const { dispatch } = this.props;
+    this.onDrop = files => {
       dispatch(uploadFile(files[0]));
       return true;
-    }
+    };
   }
 
   render() {
@@ -22,7 +23,7 @@ class UploadZone extends Component {
           <div>Drop some files here, or click and select files to upload</div>
         </Dropzone>
       </div>
-    )
+    );
   }
 }
 
@@ -30,4 +31,4 @@ UploadZone.propTypes = {
   dispatch: PropTypes.func.isRequired
 };
 
-export default connect() (UploadZone)
+export default connect()(UploadZone);
