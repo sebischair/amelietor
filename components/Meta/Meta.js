8983acfee4d060b14c6616f46ff8751c997136a5
Jiping Xu <xujiping2003@gmail.com>
2018-04-26 22:17:36 +0200

    #19: expandable meta information div

--- components/Meta/Meta.js
+++ components/Meta/Meta.js
@@ -1,20 +1,37 @@
-import React, { PropTypes, Component} from 'react'
+import React, { PropTypes, Component } from 'react';
 
 class Meta extends Component {
-
   constructor(props) {
     super(props);
-
+    this.state = {
+      open: false
+    };
   }
 
-  render(){
-    const {info} = this.props  ;
-    return (
-        <section>
-          <div className="content">{info.data || "No meta"}</div>
-        </section>
-    )
+  toggleRow = () => {
+    this.setState({ open: !this.state.open });
   };
+
+  render() {
+    const { info } = this.props;
+    // Display at most 250 characters. Click on 'show more' to see full text.
+    const limit = 250;
+    const meta = info.data ? info.data : 'No meta information available.';
+    let metaDiv;
+    if (meta.length > limit) {
+      metaDiv = (
+        <div className="content">
+          {this.state.open ? meta : meta.slice(0, limit).concat('... ')}
+          <a href="#" onClick={this.toggleRow}>
+            {this.state.open ? 'Show less' : 'Show more'}
+          </a>
+        </div>
+      );
+    } else {
+      metaDiv = <div className="content">{meta}</div>;
+    }
+    return <section>{metaDiv}</section>;
+  }
 }
 
 Meta.propTypes = {
@@ -24,9 +41,6 @@ Meta.propTypes = {
     didInvalidate: PropTypes.bool.isRequired,
     lastUpdated: PropTypes.number
   }).isRequired
-
 };
 
 export default Meta;
-
-
